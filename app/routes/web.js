244631c63dec301f7e99cdf01b555096614881d0
elainabaobao <lingchen@bu.edu>
2017-04-23 01:54:07 -0400

    Update web.js
    check auth. and check if login

--- app/routes/web.js
+++ app/routes/web.js
@@ -1,1 +1,13 @@
-routes
+function checkAuth(req, res, next) {
+    if (req.session.user)
+        return next();
+    else
+        res.redirect('/');
+}
+
+function checkIsLoggedIn(req, res, next) {
+    if (req.session.user)
+        res.redirect('/dashboard');
+    else
+        next();
+}
