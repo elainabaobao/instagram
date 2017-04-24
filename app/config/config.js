b4326046228394293eb32117dd774f5c97bda01d
Andrew Lee <Andrew Lee>
2017-04-23 21:19:50 -0400

    config folder update

--- app/config/config.js
+++ app/config/config.js
@@ -1,1 +1,8 @@
-config.js
+module.exports = function(app)
+{
+    app.set('database', 'clone.db');
+
+    app.set('port', '4000');
+    app.set('sessionSecretKey', '7d239291636c99278112d6cb58c54aaf');
+    app.set('webUrl','/');
+}
